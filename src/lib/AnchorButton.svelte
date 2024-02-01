dd608543de9f54ebf1fbbe65d90a82cda5723036
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-01 14:44:49 -0600

    Added Young Serif font

--- src/lib/AnchorButton.svelte
+++ src/lib/AnchorButton.svelte
@@ -10,18 +10,22 @@
 <style>
   a {
     border-radius: 8px;
-    border: 1px solid transparent;
+    border: 3px solid transparent;
     padding: 0.6em 1.2em;
     margin: 0 0.4em;
     font-size: 1em;
     font-weight: 500;
     font-family: inherit;
     background-color: var(--color-button-bg);
+    color: var(--color-a);
     cursor: pointer;
-    transition: border-color 0.25s;
+    transition:
+      border-color 0.25s,
+      color 0.25s;
   }
   a:hover {
     border-color: var(--color-button-border);
+    color: var(--color-a-hover);
   }
   a:focus,
   a:focus-visible {
