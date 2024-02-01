441c8352ffccbe9ae731a2cd5a0ad4a3d352370c
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-01 10:36:22 -0600

    Changed site to be Svelte

--- .eleventy.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("webgl-demo.js");
-    eleventyConfig.addPassthroughCopy("index.js");
-};
---
+++ src/App.svelte
@@ -0,0 +1,5 @@
+<main>
+  <h1>Matthew Carroll</h1>
+
+  <p>Computer Science Undergraduate</p>
+</main>
