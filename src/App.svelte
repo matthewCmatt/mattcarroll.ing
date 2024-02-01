f6dc8c9a0c886840bc673a084f2e41ea6091c8f6
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-01 12:07:57 -0600

    Added email, github, and linkedin links

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,15 @@
+<script>
+  let github = "https://github.com/matthewCmatt";
+  let linkedin = "https://www.linkedin.com/in/matthew-carroll-ab9129182/";
+  let email = "matthewCmatt@gmail.com";
+</script>
+
 <main>
   <h1>Matthew Carroll</h1>
-
   <p>Computer Science Undergraduate</p>
+  <p>
+    <a href="mailto:{email}">{email}</a>
+    - <a href={github}>GitHub</a>
+    - <a href={linkedin}>LinkedIn</a>
+  </p>
 </main>
