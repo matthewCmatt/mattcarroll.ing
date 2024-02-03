9cea3a9a9040e9a4e85419bb4b1fc5dcf508a1aa
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-02 21:41:55 -0600

    Added transparent .ing to title

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script>
   import AnchorButton from "./lib/AnchorButton.svelte";
+  import TransparentTitle from "./lib/TransparentTitle.svelte";
 
   let github = "https://github.com/matthewCmatt";
   let linkedin = "https://www.linkedin.com/in/matthew-carroll-ab9129182/";
 </script>
 
 <main>
-  <h1>Matthew Carroll</h1>
+  <h1>
+    Matthew Carroll<TransparentTitle>.ing</TransparentTitle>
+  </h1>
   <p>Computer Science Undergraduate at the University of Oklahoma</p>
   <AnchorButton href={github} label="GitHub" />
   <AnchorButton href={linkedin} label="LinkedIn" />
---
+++ src/lib/TransparentTitle.svelte
@@ -0,0 +1,21 @@
+<div><slot /></div>
+
+<style>
+    div {
+        position: absolute;
+        display: inline;
+        opacity: 50%;
+        background: -webkit-linear-gradient(
+            var(--color-a),
+            var(--color-a-hover)
+        );
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+
+    @media (prefers-color-scheme: light) {
+        div {
+            opacity: 70%;
+        }
+    }
+</style>
