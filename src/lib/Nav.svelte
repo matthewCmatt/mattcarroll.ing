cbef86b0b1901ee94d2fc226aa16041918b995ea
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-05 18:59:16 -0600

    Change Nav links to JS

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,19 +1,19 @@
 <script>
-    import { Link } from "svelte-routing";
     import { links } from "svelte-routing";
     import AnchorButton from "/src/lib/AnchorButton.svelte";
 
+    export let excludeHome = false;
+    let home = "/";
+    let projects = "/projects";
     let github = "https://github.com/matthewCmatt";
     let linkedin = "https://www.linkedin.com/in/matthew-carroll-ab9129182/";
-
-    export let excludeHome = false;
 </script>
 
 <nav use:links>
     {#if !excludeHome}
-        <AnchorButton href="/" label="Home" />
+        <AnchorButton href={home} label="Home" />
     {/if}
-    <AnchorButton href="/projects" label="Projects" />
+    <AnchorButton href={projects} label="Projects" />
     <AnchorButton href={github} label="GitHub" ext />
     <AnchorButton href={linkedin} label="LinkedIn" ext />
 </nav>
