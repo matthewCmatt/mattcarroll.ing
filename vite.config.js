caddc4426b4e9234818e4f6aae9f51be4431baba
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-08 00:57:17 -0600

    Added webp image transform

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { imagetools } from 'vite-imagetools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    imagetools({
+      defaultDirectives: (url) => {
+        return new URLSearchParams({
+          format: "webp"
+        })
+      }
+    }),
+    svelte()
+  ],
 })
