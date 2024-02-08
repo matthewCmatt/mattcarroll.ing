bf092e762f0c3945b22211d3b83d370582abfedd
Matthew Carroll <matthewCmatt@gmail.com>
2024-02-08 01:03:38 -0600

    Revert "Added webp image transform"
    This reverts commit caddc4426b4e9234818e4f6aae9f51be4431baba.

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { imagetools } from 'vite-imagetools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    imagetools({
-      defaultDirectives: (url) => {
-        return new URLSearchParams({
-          format: "webp"
-        })
-      }
-    }),
-    svelte()
-  ],
+  plugins: [svelte()],
 })
