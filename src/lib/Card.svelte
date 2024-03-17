3260910adfbb708f81a7462ac41f0627fb70324d
Matthew Carroll <matthewCmatt@gmail.com>
2024-03-17 06:12:40 -0500

    fix: link card uses anchor tag instead of svelte router

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,66 +1,70 @@
 <script>
-    import Modal from "./Modal.svelte";
-    import { navigate } from "svelte-routing";
+  import Modal from "./Modal.svelte";
+  import { navigate } from "svelte-routing";
 
-    export let image_src = "";
-    export let image_alt = "";
-    export let title = "Card Title";
-    export let link = false;
+  export let image_src = "";
+  export let image_alt = "";
+  export let title = "Card Title";
+  export let link = false;
 
-    function navigateTo() {
-        navigate(link);
-    }
-    let showModal;
-    $: onClick = link ? navigateTo : showModal;
+  let showModal;
 </script>
 
-<button on:click={onClick} class="card hoverable">
-    {#if image_src}
-        <img class="cover" src={image_src} alt={image_alt} />
-    {/if}
+{#if !link}
+  <button on:click={showModal} class="card hoverable">
+    <img class="cover" src={image_src} alt={image_alt} />
     <h2>
-        {title}
+      {title}
     </h2>
-</button>
+  </button>
+{:else}
+  <a href={link}>
+    <button class="card hoverable">
+      <h2>
+        {title}
+      </h2>
+    </button>
+  </a>
+{/if}
 
 <Modal bind:showModal>
-    <img class="cover" src={image_src} alt={image_alt} />
-    <h2>{title}</h2>
-    <slot />
+  <img class="cover" src={image_src} alt={image_alt} />
+  <h2>{title}</h2>
+  <slot />
 </Modal>
 
 <style>
-    .card {
-        --border-radius: 5px;
-        --offset: max(1em, var(--border-radius));
+  .card {
+    --border-radius: 5px;
+    --offset: max(1em, var(--border-radius));
 
-        width: 50em;
+    width: 50em;
 
-        background-color: var(--color-button-bg);
-        border-radius: var(--border-radius);
-        font-family: inherit;
+    background-color: var(--color-button-bg);
+    border-radius: var(--border-radius);
+    font-family: inherit;
 
-        overflow: hidden;
+    overflow: hidden;
 
-        padding: var(--offset);
-        margin: 1.5em;
+    padding: var(--offset);
+    margin: 1.5em;
 
-        transition: 0.25s;
-    }
+    transition: 0.25s;
+  }
 
-    .card:hover {
-        box-shadow: 0px 2px 10px 10px rgba(0, 0, 0, 0.2);
-    }
+  .card:hover {
+    box-shadow: 0px 2px 10px 10px rgba(0, 0, 0, 0.2);
+  }
 
-    button.card {
-        display: block;
-    }
+  button.card {
+    display: block;
+  }
 
-    .cover {
-        width: calc(100% + 2 * var(--offset));
-        margin-left: calc(-1 * var(--offset));
-        margin-top: calc(-1 * var(--offset));
-        object-fit: cover;
-        aspect-ratio: 3 / 2;
-    }
+  .cover {
+    width: calc(100% + 2 * var(--offset));
+    margin-left: calc(-1 * var(--offset));
+    margin-top: calc(-1 * var(--offset));
+    object-fit: cover;
+    aspect-ratio: 3 / 2;
+  }
 </style>
