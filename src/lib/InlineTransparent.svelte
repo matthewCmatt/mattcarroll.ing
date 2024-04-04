559f47def87ad6d3d91008426dec4ecf3b1d3b06
Matthew Carroll <matthewCmatt@gmail.com>
2024-04-04 14:09:12 -0500

    feat: recolor anchor

--- src/lib/InlineTransparent.svelte
+++ src/lib/InlineTransparent.svelte
@@ -1,24 +1,21 @@
 <div>
-    <slot />
+	<slot />
 </div>
 
 <style>
-    div {
-        position: absolute;
-        display: inline;
-        opacity: 50%;
-        background: -webkit-linear-gradient(
-            var(--color-a),
-            var(--color-a-hover)
-        );
-        background-clip: text;
-        color: rgba(0, 0, 0, 0);
-        user-select: none;
-    }
+	div {
+		position: absolute;
+		display: inline;
+		opacity: 50%;
+		background: -webkit-linear-gradient(var(--color-a-hover), var(--color-a));
+		background-clip: text;
+		color: rgba(0, 0, 0, 0);
+		user-select: none;
+	}
 
-    @media (prefers-color-scheme: light) {
-        div {
-            opacity: 70%;
-        }
-    }
+	@media (prefers-color-scheme: light) {
+		div {
+			opacity: 70%;
+		}
+	}
 </style>
